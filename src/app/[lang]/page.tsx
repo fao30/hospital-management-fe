f14d067a9b1153ed33acb500edf3c081f8d03252
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-05 20:36:00 +0700

    fix: antd

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,5 @@
-import { getServerAuthSession } from "@/api/auth";
-import { api } from "@/trpc/server";
 import { Fragment } from "react";
 
 export default async function HomePage() {
-  const session = await getServerAuthSession();
-  const data = await api.visit.list.query();
-
-  return <Fragment>page</Fragment>;
+  return <Fragment>Home</Fragment>;
 }
