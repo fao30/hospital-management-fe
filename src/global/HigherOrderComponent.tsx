2105bfe6748d92c393c7b2c6c9907bbb9674eae6
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-09 02:16:03 +0700

    adjust

--- src/global/HigherOrderComponent.tsx
+++ src/global/HigherOrderComponent.tsx
@@ -13,24 +13,21 @@ type Props = { isTokenValid?: boolean; session: Session | null; t: Dictionary; l
 
 export default function HigherOrderComponent({ isTokenValid, session, t, lang, children }: Props) {
   const { setT, setSession, setLang } = useStore();
-
   const pathname = usePathname();
 
-  useEffect(() => {
-    if (isTokenValid === false && session) {
-      signOut().catch((err) => console.error(err));
-    }
-  }, [isTokenValid, session]);
+  if (isTokenValid === false && session) {
+    signOut().catch((err) => console.error(err));
+  }
+
+  if (session && pathname.includes("dashboard")) {
+    if (MENU_ITEMS_TO_REMOVE[session.user.role_id].includes(getSelectedMenu(pathname).at(0)!)) redirect(`/${lang}/dashboard`);
+  }
 
   useEffect(() => {
     if (session) setSession(session);
     if (t) setT(t);
     if (lang) setLang(lang);
   }, [session, t, lang]);
-
-  if (session && pathname.includes("dashboard")) {
-    if (MENU_ITEMS_TO_REMOVE[session.user.role_id].includes(getSelectedMenu(pathname).at(0)!)) redirect(`/${lang}/dashboard`);
-  }
 
   return children;
 }
