7936794a113976500ae8362052fe90b9225c147e
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-15 09:43:21 +0700

    tanstack-query devtools

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -3,6 +3,7 @@
 import { type AppRouter } from "@/api/root";
 import { getUrl, transformer } from "@/trpc/shared";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { unstable_httpBatchStreamLink as httpBatchStreamLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
@@ -30,6 +31,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode; cookies: s
   return (
     <QueryClientProvider client={queryClient}>
       <api.Provider client={trpcClient} queryClient={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         {props.children}
       </api.Provider>
     </QueryClientProvider>
